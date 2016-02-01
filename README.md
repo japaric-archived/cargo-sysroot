d0eb8a1f7887318ce449f6de456d3bc43c17f8ea
Jorge Aparicio <japaricious@gmail.com>
2016-02-01 16:48:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,151 @@
+# `cargo-sysroot`
+
+> Builds a sysroot with cross compiled standard crates
+
+## The problem
+
+Let's say you are building a `no_std` crate for a custom target, e.g. some Cortex M microcontroller.
+You make that crate depend on [`rust-libcore`] to have Cargo cross compile the `core` crate as
+part of the build process. `cargo build --target=$triple` works fine, and you get your cross
+compiled crate.
+
+[rust-libcore]: https://crates.io/crates/rust-libcore
+
+Now let's say you want to depend on another `no_std` crate, like [`spin`], so you add it to your
+dependencies, and call `cargo build --target=$target`. But you get:
+
+[spin]: https://crates.io/crates/spin/
+
+``` rust
+   Compiling spin v0.3.5
+error: can't find crate for `core` [E0463]
+error: aborting due to previous error
+```
+
+`rust-libcore` builds a `core` crate that can be used by your crate, but not by `spin`. When Cargo
+builds `spin`, it looks for the `core` crate in your Rust installation but there's none in it.
+
+You have two alternatives:
+
+You make the `spin` crate depend on `rust-libcore` and things just work. But, IMO, this is a bad
+approach because (1) it's "viral", you'll have to make all the crates you want to use depend on
+`rust-libcore`, and (2) if your crate depends on any other standard crate then more crates like
+`rust-libstd` would need to be created.
+
+Or you could install the cross compiled `core` crate in your Rust installation path. But, you may
+not be able to due to permissions, or may not want to pollute your Rust installation.
+
+## The solution
+
+Enter `--sysroot`, this undocumented `rustc` feature let's you override the library search path.
+This means you can create a "sysroot", which is like a minimal Rust installation with only
+libraries, populate it with cross compiled crates and instruct `rustc` to use that sysroot instead
+of the default Rust installation.
+
+And `crate sysroot` does this for you. It takes cares of creating a sysroot with cross compiled
+standard crates:
+
+``` rust
+$ cargo sysroot --target $target target/sysroot
+INFO: fetching source tarball
+INFO: unpacking source tarball
+INFO: creating .commit-hash file
+INFO: symlinking host crates
+INFO: building the core crate
+   Compiling core v0.0.0 (file://...)
+INFO: copy the core crate to the sysroot
+
+$ tree target/sysroot
+target/sysroot
+├── lib
+│   └── rustlib
+│       ├── $target
+│       │   └── lib
+│       │       └── libcore.rlib
+│       └── $host -> $RUST_INSTALLATION/lib/rustlib/$host
+└── src
+    ├── libcore
+    │   │── lib.rs
+    │   └── (...)
+    ├── libstd
+    │   │── lib.rs
+    │   └── (...)
+    └── (...)
+```
+
+With the sysroot in place you can use (*) the `RUSTFLAGS` env variable to make Cargo use the
+sysroot:
+
+(*) Support for `RUSTFLAGS` has not yet landed. See [rust-lang/cargo#2241].
+
+[rust-lang/cargo#2241]: https://github.com/rust-lang/cargo/pull/2241
+
+```
+$ RUSTFLAGS='--sysroot target/sysroot' cargo build --target $triple
+   Compiling spin v0.3.5
+   Compiling $crate v0.1.0 (file://...)
+```
+
+## Future
+
+Right now `cargo sysroot` only cross compiles the `core` crate, but in the future it will be able
+to compile any standard crate. This would make it easier to target systems where the Rust team
+doesn't [provide] cross compiled standard crates. Rust's [new Cargo-based build system] will make
+this easier to implement.
+
+[provide]: http://static.rust-lang.org/dist/
+[new Cargo-based build system]: https://github.com/rust-lang/rust/pull/31123
+
+Once the standard crates become build-able with Cargo, I expect that they'll start exposing Cargo
+features to allow customization.  I'm envisioning having `cargo sysroot` look at a `sysroot.toml`
+file  that specifies the Cargo features each standard crate will be compiled with. Something like
+this:
+
+``` toml
+# Customize the std crate for the mips-musl target
+[mips-unknown-linux-musl.std]
+# disables both backtraces, and statically linked jemalloc
+default-features = false
+# enables dynamically linked jemalloc
+features = ["dynamic-jemalloc"]
+```
+
+`cargo-sysroot` will also need to cross compile C dependencies like `compiler-rt` and `jemalloc`.
+The `sysroot.toml` could also expose keys that specify which cross compiler tools to use:
+
+``` toml
+[mips-unknown-linux-musl.build]
+ar = "mips-openwrt-linux-ar"
+gcc = "mips-openwrt-linux-gcc"
+ranlib = "mips-openwrt-linux-ranlib"
+```
+
+Because `cargo-sysroot` is not coupled to the `cargo build` process, it would be possible to build
+the sysroot using the nightly channel, and then use the sysroot with the stable channel.
+`cargo-sysroot` would need to grow an option to build an specific checkout of Rust source, right now
+it always checkout the source at the commit-hash provided by `rustc -Vv`.
+
+I'd like to better integrate `cargo-sysroot` with other tools. I know that `multirust` [plans] to
+provide a command to fetch Rust source code, `cargo-sysroot` could use the source code fetched by
+`multirust` instead of fetching the code itself. And I'd like `cargo-sysroot` to become more
+"transparent", instead of having to set an environment variable, I'd like to integrate it with Cargo
+via `.cargo/config` to make Cargo pass `--sysroot` to `rustc` automatically, and even pick a
+different sysroot based on the profile (debug vs release).
+
+[plans]: https://github.com/brson/multirust/issues/77
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the
+work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any
+additional terms or conditions.
---
+++ src/main.rs
@@ -0,0 +1,217 @@
+// #![deny(warnings)]
+
+extern crate clap;
+extern crate fern;
+extern crate rustc_version;
+extern crate tempdir;
+
+#[macro_use]
+extern crate log;
+
+use std::fs::{self, File};
+use std::io::{Read, Write};
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+use clap::{App, AppSettings, Arg, SubCommand};
+
+// TODO proper error reporting
+macro_rules! try {
+    ($e:expr) => {
+        $e.unwrap_or_else(|_| panic!(stringify!($e)))
+    }
+}
+
+struct Context<'a> {
+    commit_hash: &'a str,
+    host: &'a str,
+    out_dir: &'a Path,
+    release: bool,
+    target: &'a str,
+}
+
+fn main() {
+    let rustc_version::VersionMeta { ref host, ref commit_hash, .. } =
+        rustc_version::version_meta();
+    let commit_hash = commit_hash.as_ref().unwrap();
+
+    let ref matches = App::new("cargo-sysroot")
+                          .bin_name("cargo")
+                          .settings(&[AppSettings::SubcommandRequired])
+                          .subcommand(SubCommand::with_name("sysroot")
+                                          .about("Builds a sysroot with cross compiled standard \
+                                                  crates")
+                                          .arg(Arg::with_name("triple")
+                                                   .help("Target triple to compile for")
+                                                   .long("target")
+                                                   .required(true)
+                                                   .takes_value(true))
+                                          .arg(Arg::with_name("release")
+                                                   .help("Build artifacts in release mode, \
+                                                          with optimizations")
+                                                   .long("release"))
+                                          .arg(Arg::with_name("out_dir")
+                                                   .help("Output directory")
+                                                   .required(true))
+                                          .author("Jorge Aparicio <japaricious@gmail.com>"))
+                          .version(env!("CARGO_PKG_VERSION"))
+                          .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("sysroot") {
+        if let (Some(target), Some(out_dir)) = (matches.value_of("triple"),
+                                                matches.value_of("out_dir")) {
+            if host == target {
+                panic!("`cargo sysroot` for host has not been implement yet");
+            }
+
+            let ref ctx = Context {
+                commit_hash: commit_hash,
+                host: host,
+                out_dir: Path::new(out_dir),
+                release: matches.is_present("release"),
+                target: target,
+            };
+
+            init_logger();
+            fetch_source(ctx);
+            symlink_host_crates(ctx);
+            build_target_crates(ctx);
+        }
+    }
+}
+
+fn init_logger() {
+    let config = fern::DispatchConfig {
+        format: Box::new(|msg, level, _| format!("{}: {}", level, msg)),
+        output: vec![fern::OutputConfig::stderr()],
+        level: log::LogLevelFilter::Trace,
+    };
+
+    try!(fern::init_global_logger(config, log::LogLevelFilter::Trace));
+}
+
+fn fetch_source(ctx: &Context) {
+    let hash = ctx.commit_hash;
+    let ref src_dir = ctx.out_dir.join("src");
+    let ref hash_file = src_dir.join(".commit-hash");
+
+    if hash_file.exists() {
+        let ref mut old_hash = String::with_capacity(hash.len());
+        try!(try!(File::open(hash_file)).read_to_string(old_hash));
+
+        if old_hash == hash {
+            info!("source up to date");
+            return;
+        }
+    }
+
+    if src_dir.exists() {
+        info!("purging the src directory");
+        try!(fs::remove_dir_all(src_dir));
+    }
+
+    try!(fs::create_dir_all(src_dir));
+
+    info!("fetching source tarball");
+    let ref curl = try!(Command::new("curl")
+                            .arg("-L")
+                            .arg(format!("https://github.com/rust-lang/rust/tarball/{}", hash))
+                            .output());
+
+    assert!(curl.status.success());
+
+    info!("unpacking source tarball");
+    let short_hash = &hash[..7];
+    let ref mut tar = try!(Command::new("tar")
+                               .args(&["-xz", "--strip-components", "2", "-C"])
+                               .arg(src_dir)
+                               .arg(format!("rust-lang-rust-{}/src", short_hash))
+                               .stdin(Stdio::piped())
+                               .spawn());
+    try!(tar.stdin.as_mut().unwrap().write_all(&curl.stdout));
+
+    info!("creating .commit-hash file");
+    try!(try!(File::create(hash_file)).write_all(hash.as_bytes()));
+}
+
+fn symlink_host_crates(ctx: &Context) {
+    info!("symlinking host crates");
+
+    let sys_root = try!(String::from_utf8(try!(Command::new("rustc")
+                                                   .args(&["--print", "sysroot"])
+                                                   .output())
+                                              .stdout));
+    let ref src = Path::new(sys_root.trim_right()).join(format!("lib/rustlib/{}", ctx.host));
+    let ref rustlib_dir = ctx.out_dir.join("lib/rustlib");
+    let ref dst = rustlib_dir.join(ctx.host);
+
+    try!(fs::create_dir_all(rustlib_dir));
+
+    if dst.exists() {
+        try!(fs::remove_file(dst));
+    }
+
+    match () {
+        #[cfg(unix)]
+        () => {
+            use std::os::unix::fs;
+
+            try!(fs::symlink(src, dst))
+        }
+        #[cfg(windows)]
+        () => {
+            use std::os::windaws::fs;
+
+            try!(fs::symlink_dir(src, dst))
+        }
+    }
+}
+
+// XXX we only build the core crate for now
+// TODO build the rest of crates
+fn build_target_crates(ctx: &Context) {
+    const CARGO_TOML: &'static str = r#"[package]
+authors = ["The Rust Project Developers"]
+name = "core"
+version = "0.0.0"
+
+[lib]
+name = "core"
+path = "lib.rs""#;
+
+    let ref src_dir = ctx.out_dir.join("src/libcore");
+
+    try!(try!(File::create(src_dir.join("Cargo.toml"))).write_all(CARGO_TOML.as_bytes()));
+
+    let ref temp_dir = try!(tempdir::TempDir::new("core"));
+    let temp_dir = temp_dir.path();
+
+    info!("building the core crate");
+    assert!(try!(Command::new("cargo")
+                     .args(&["build", "--target"])
+                     .arg(ctx.target)
+                     .arg(if ctx.release {
+                         "--release"
+                     } else {
+                         // XXX dummy value
+                         "--lib"
+                     })
+                     .current_dir(src_dir)
+                     .env("CARGO_TARGET_DIR", temp_dir)
+                     .status())
+                .success());
+
+    info!("copy the core crate to the sysroot");
+    let ref libdir = ctx.out_dir.join(format!("lib/rustlib/{}/lib", ctx.target));
+    try!(fs::create_dir_all(libdir));
+
+    let ref src = temp_dir.join(format!("{}/{}/libcore.rlib",
+                                        ctx.target,
+                                        if ctx.release {
+                                            "release"
+                                        } else {
+                                            "debug"
+                                        }));
+    let ref dst = libdir.join("libcore.rlib");
+    try!(fs::copy(src, dst));
+}
