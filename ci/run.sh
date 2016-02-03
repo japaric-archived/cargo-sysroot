9a230a1f6ff353a67c91d4bdace27a442031101a
Jorge Aparicio <japaricious@gmail.com>
2016-02-03 14:59:19 -0500

    feat: support --target path/to/target.json

---
+++ ci/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+cargo install --path .
+
+# Test with supported target
+cargo sysroot --target arm-unknown-linux-gnueabihf sysroot --verbose
+tree sysroot
+rm -r sysroot
+
+# Test with custom target
+cat >custom.json <<EOF
+{
+  "arch": "arm",
+  "llvm-target": "thumbv7m-none-eabi",
+  "os": "none",
+  "target-endian": "little",
+  "target-pointer-width": "32"
+}
+EOF
+cp custom.json ..
+cargo sysroot --target ../custom.json sysroot --verbose
+tree sysroot
+rm -r sysroot
+rm custom.json
